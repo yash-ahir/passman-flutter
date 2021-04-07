c59f92d4222f95b4ebc104fc6213fbf1efd8d5c3
yash-ahir <yashsorathiya25@gmail.com>
2021-04-07 22:18:27 +0530

    Added documentation, made all methods asynchronous as to be non-blocking, refactored hash method to utilize sha256 with 50000 iterations of pbkdf2 instead of sha512 with 100100 iterations for performance reasons.

--- lib/crypt.dart
+++ lib/crypt.dart
@@ -8,29 +8,44 @@ class Crypt {
 
   Crypt({@required this.masterPass});
 
-  Map<String, String> encrypt({@required String plainText}) {
+  /// The decrypt method takes the plaintext as input and returns a map of
+  /// base64 encoded ciphertext and initialization vector.
+  Future<Map<String, String>> encrypt({@required String plainText}) async {
+    /// Pad master password to 32 bytes for optimal key length.
     final key = EncryptLib.Key.fromUtf8(this.masterPass.padRight(32));
+
+    /// Generate a securely random initialization vector of 16 bytes.
     final iv = EncryptLib.IV.fromSecureRandom(16);
 
+    /// Instantiate an Encrypter with AES CBC as the mode of operation.
     final encrypter = EncryptLib.Encrypter(
       EncryptLib.AES(key, mode: EncryptLib.AESMode.cbc),
     );
 
+    /// Encrypt the plaintext with the previously generated initialization vector.
     final cipherText = encrypter.encrypt(plainText, iv: iv);
 
+    /// Encode ciphertext and initialization vector to base64 for storage.
     return {
       "cipherText": cipherText.base64,
       "iv": iv.base64,
     };
   }
 
-  String decrypt({@required String cipherText, @required String iv}) {
+  /// The decrypt method takes the base64 encoded ciphertext and initialization vector,
+  /// and returns the original plaintext.
+  Future<String> decrypt(
+      {@required String cipherText, @required String iv}) async {
+    /// Pad master password to 32 bytes for optimal key length.
     final key = EncryptLib.Key.fromUtf8(this.masterPass.padRight(32));
 
+    /// Instantiate an Encrypter with AES CBC as the mode of operation.
     final encrypter = EncryptLib.Encrypter(
       EncryptLib.AES(key, mode: EncryptLib.AESMode.cbc),
     );
 
+    /// Decrypt the ciphertext with after decoding both it and the initialization vector
+    /// back to utf8 from base64.
     final plainText = encrypter.decrypt(
       EncryptLib.Encrypted.fromBase64(cipherText),
       iv: EncryptLib.IV.fromBase64(iv),
@@ -39,11 +54,17 @@ class Crypt {
     return plainText;
   }
 
-  Map<String, String> hash() {
+  /// The hash method takes the master password and hashes it with 50000 rounds
+  /// of PBKDF2 with SHA256 as HMAC. It returns a map of base64 encoded
+  /// hashed master password and salt.
+  ///
+  /// Note that to prevent the UI thread from being blocked, this method should
+  /// always be offloaded to a separate isolate with the [compute] function.
+  static Future<Map<String, String>> hash(String masterPass) async {
     final salt = EncryptLib.IV.fromSecureRandom(16);
 
     final hashedMasterPass = HashLib.PBKDF2
-        .hmac_sha512(utf8.encode(this.masterPass), salt.bytes, 100100, 64);
+        .hmac_sha256(utf8.encode(masterPass), salt.bytes, 50000, 32);
 
     return {
       "hashedMasterPass": base64.encode(hashedMasterPass),
