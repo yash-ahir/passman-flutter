4fe84b0dc97e3ef3b05301d1406d6ea8053bfabc
yash-ahir <yashsorathiya25@gmail.com>
2021-04-16 02:02:28 +0530

    Added dynamically loading and decrypting credentials from database, wrapped widget with EasyDynamicTheme for dynamic theming with shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,53 @@
+import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:passman/routes/home_route.dart';
 import 'package:passman/routes/credential_route.dart';
 import 'package:passman/routes/settings_route.dart';
+import 'package:passman/services/credential_database.dart';
 import 'package:passman/themes/theme.dart';
+import 'package:provider/provider.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      EasyDynamicThemeWidget(child: MyApp()),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String title = "PassMan";
 
-    return NeumorphicApp(
-      theme: NeumorphicThemes.lightTheme,
-      darkTheme: NeumorphicThemes.darkTheme,
-      themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: false,
-      title: title,
-      routes: {
-        SettingsRoute.routeName: (ctx) => SettingsRoute(),
-        CredentialRoute.routeName: (ctx) => CredentialRoute(),
-      },
-      home: HomeRoute(
+    return Provider(
+      create: (_) => AppDatabase(),
+      child: NeumorphicApp(
+        theme: NeumorphicThemes.lightTheme,
+        darkTheme: NeumorphicThemes.darkTheme,
+        themeMode: EasyDynamicTheme.of(context).themeMode,
+        debugShowCheckedModeBanner: false,
         title: title,
+        routes: {
+          SettingsRoute.routeName: (ctx) => SettingsRoute(),
+          CredentialRoute.routeName: (ctx) => CredentialRoute(),
+        },
+        home: HomeRoute(
+          title: title,
+        ),
+        builder: (context, widget) {
+          return ResponsiveWrapper.builder(
+            widget,
+            minWidth: 450,
+            backgroundColor: Color(0x00),
+            defaultScale: true,
+            breakpoints: [
+              ResponsiveBreakpoint.resize(450, name: MOBILE),
+              ResponsiveBreakpoint.autoScale(800, name: TABLET),
+              ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+              ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+              ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+            ],
+          );
+        },
       ),
-      builder: (context, widget) {
-        return ResponsiveWrapper.builder(
-          widget,
-          minWidth: 450,
-          backgroundColor: Color(0x00),
-          defaultScale: true,
-          breakpoints: [
-            ResponsiveBreakpoint.resize(450, name: MOBILE),
-            ResponsiveBreakpoint.autoScale(800, name: TABLET),
-            ResponsiveBreakpoint.autoScale(1000, name: TABLET),
-            ResponsiveBreakpoint.resize(1200, name: DESKTOP),
-            ResponsiveBreakpoint.autoScale(2460, name: "4K"),
-          ],
-        );
-      },
     );
   }
 }
