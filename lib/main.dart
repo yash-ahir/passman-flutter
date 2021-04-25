e19682b7572dc38ea71d788ffc3fcd167379a4ec
yash-ahir <yashsorathiya25@gmail.com>
2021-04-25 23:57:42 +0530

    Added provider for UnhashedPassword

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:passman/models/unhashed_password.dart';
 import 'package:passman/routes/credential_route.dart';
 import 'package:passman/routes/home_route.dart';
 import 'package:passman/routes/lock_screen_route.dart';
 import 'package:passman/routes/settings_route.dart';
 import 'package:passman/services/app_locker.dart';
-import 'package:passman/services/credential_database.dart';
+import 'package:passman/services/database.dart';
 import 'package:passman/themes/theme.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_framework/responsive_framework.dart';
@@ -22,8 +23,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final String _title = "PassMan";
 
-    return Provider(
-      create: (_) => AppDatabase(),
+    return MultiProvider(
+      providers: [
+        Provider<AppDatabase>(
+          create: (_) => AppDatabase(),
+        ),
+        ChangeNotifierProvider<UnhashedPassword>(
+          create: (_) => UnhashedPassword(),
+        ),
+      ],
       child: NeumorphicApp(
         theme: NeumorphicThemes.lightTheme,
         darkTheme: NeumorphicThemes.darkTheme,
