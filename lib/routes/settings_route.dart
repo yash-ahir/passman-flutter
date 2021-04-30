4fc658ba7c80973b538e10651c4759d6ed88e6a9
yash-ahir <yashsorathiya25@gmail.com>
2021-04-30 22:34:37 +0530

    Added developer information

--- lib/routes/settings_route.dart
+++ lib/routes/settings_route.dart
@@ -1,11 +1,8 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:passman/widgets/neumorphic_icon_button.dart';
+import 'package:passman/widgets/neumorphic_text_button.dart';
 import 'package:passman/widgets/system_theme_picker.dart';
-
-// FOR DEVELOPMENT
-import 'package:provider/provider.dart';
-import 'package:passman/services/database.dart';
-import 'package:moor_db_viewer/moor_db_viewer.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsRoute extends StatefulWidget {
   static String routeName = "/settings-route";
@@ -15,6 +12,9 @@ class SettingsRoute extends StatefulWidget {
 }
 
 class _SettingsRouteState extends State<SettingsRoute> {
+  final _defaultInnerPadding = 15.0;
+  final _defaultOuterPadding = 10.0;
+
   @override
   Widget build(BuildContext context) {
     final NeumorphicAppBar appBar = NeumorphicAppBar(
@@ -30,31 +30,66 @@ class _SettingsRouteState extends State<SettingsRoute> {
       ),
       textStyle: NeumorphicTheme.currentTheme(context).textTheme.bodyText1,
       title: Text("Settings"),
-      actions: [
-        // FOR DEVELOPMENT
-        NeumorphicIconButton(
-          icon: Icon(
-            Icons.developer_mode,
-            color: NeumorphicTheme.accentColor(context),
-          ),
-          tooltip: "View table",
-          onPressed: () {
-            final database = Provider.of<AppDatabase>(context, listen: false);
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: (ctx) => MoorDbViewer(database)));
-          },
-        ),
-      ],
     );
 
     return Scaffold(
       appBar: appBar,
-      body: Column(
-        children: [
-          SingleChildScrollView(
-            child: SystemThemePicker(),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SystemThemePicker(),
+            SizedBox(
+              height: 50.0,
+            ),
+            Text(
+              "Miscellaneous information",
+              style: NeumorphicTheme.currentTheme(context).textTheme.bodyText1,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            NeumorphicTextButton(
+              text: "Github Repository",
+              icon: Icons.source,
+              iconColor: NeumorphicTheme.accentColor(context),
+              innerPadding: _defaultInnerPadding,
+              outerPadding: _defaultOuterPadding,
+              onPressed: () async {
+                await launch("https://github.com/yash-ahir/passman-flutter");
+              },
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Text(
+              "Developed by Yash Ahir",
+              style: NeumorphicTheme.currentTheme(context).textTheme.bodyText1,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            NeumorphicTextButton(
+              text: "More about me on my website",
+              icon: Icons.person,
+              iconColor: NeumorphicTheme.accentColor(context),
+              innerPadding: _defaultInnerPadding,
+              outerPadding: _defaultOuterPadding,
+              onPressed: () async {
+                await launch("https://yashahir.tech");
+              },
+            ),
+            NeumorphicTextButton(
+              text: "Send me an E-mail",
+              icon: Icons.email,
+              iconColor: NeumorphicTheme.accentColor(context),
+              innerPadding: _defaultInnerPadding,
+              outerPadding: _defaultOuterPadding,
+              onPressed: () async {
+                await launch("mailto:email@yashahir.tech");
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
