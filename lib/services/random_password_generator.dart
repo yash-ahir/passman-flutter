811cafa84b76254aff6c68ab6bcfb54d0a70d972
yash-ahir <yashsorathiya25@gmail.com>
2021-04-30 22:35:04 +0530

    Fixed cryptographically randomly generated passwords

--- lib/services/random_password_generator.dart
+++ lib/services/random_password_generator.dart
@@ -1,14 +1,112 @@
 import 'dart:math';
-import 'dart:convert';
 import 'package:flutter/foundation.dart';
 
 class RandomPasswordGenerator {
   static final _randomGenerator = Random.secure();
 
   static String generateRandomString({@required int length}) {
-    final randomBytes =
-        List.generate(length, (index) => _randomGenerator.nextInt(256));
+    final characters = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      "~",
+      "`",
+      "!",
+      "@",
+      "#",
+      "\$",
+      "%",
+      "^",
+      "&",
+      "*",
+      "(",
+      ")",
+      "-",
+      "_",
+      "+",
+      "=",
+      "[",
+      "]",
+      "{",
+      "}",
+      "\\",
+      "|",
+      "/",
+      "?",
+      ".",
+      ">",
+      "<",
+      ":",
+      ";",
+      "'",
+      "\"",
+    ];
 
-    return base64.encode(randomBytes).substring(1, length + 1);
+    String output = "";
+
+    for (int i = 0; i < length; i++) {
+      output += characters[_randomGenerator.nextInt(characters.length)];
+    }
+
+    return output;
   }
 }
